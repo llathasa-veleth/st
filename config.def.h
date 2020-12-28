2b4e39fadca424fa83cbd60a07b24656b9257380
Llathasa Veleth <llathasa@outlook.com>
2020-12-28 16:44:48 +0300

    Updated default config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "IBM Plex Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
@@ -27,7 +27,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = .8;
 
 /*
  * word delimiter string
