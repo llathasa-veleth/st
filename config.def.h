d6d2dbb290c456c47d6e5b957c29dda3bedfc051
Llathasa Veleth <llathasa@outlook.com>
2020-12-28 16:28:40 +0300

    Updated default config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "IBM Plex Mono:pixelsize=16:antialias=true:autohint=true";
+static int borderpx = 15;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -91,29 +91,29 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 2;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#314E6D",
+	"#FF7C84",
+	"#48F896",
+	"#FFBA7C",
+	"#48AAF8",
+	"#FF7CCE",
+	"#48F8ED",
+	"#E8FDFC",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#416790",
+	"#FFAFB4",
+	"#79FAB2",
+	"#FFD5AF",
+	"#79C1FA",
+	"#FFAFE1",
+	"#79FAF2",
+	"#FFFFFF",
 
 	[255] = 0,
 
@@ -139,7 +139,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
